fcbdd5dbe0f3a7f9ca7c6376b4bda2fdd4e8f265
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-30 15:24:53 +0100

    added structure to the list

--- src/components/ListMonsters.js
+++ src/components/ListMonsters.js
@@ -3,8 +3,19 @@ import './ListMonsters.css';
 
 class ListMonsters extends React.Component {
     render () {
+        
         return (
-            <div>Hello</div>
+            <div>
+                <img src='' alt ='' />
+                <div>
+                    <p>Name</p>
+                    <p>Level</p>
+                    <p>Attack</p>
+                    <p>Defense</p>
+                    <p>Description</p>
+                    
+                </div>
+            </div>
         )
     }
 }
