39b3e3f28009c6f8077225f99d0b1d26b541e416
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-30 15:16:15 +0100

    created ListMonsters.js + linked to app

---
+++ src/components/ListMonsters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ListMonsters.css';
+
+class ListMonsters extends React.Component {
+    render () {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+
+
+
+export default ListMonsters;
