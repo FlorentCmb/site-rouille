b3eada0fe6c46108491bd19596cac766b222b778
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-31 05:49:37 +0100

    start adding flexbox properties

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -4,7 +4,24 @@ import './Payment.css'
 class Payment extends React.Component {
     render () {
         return(
-            <div>
+            <div className ='listPayment'>
+                <div className ='listOffers'>
+                    <p>Bloody deal</p>
+                    <p>20 currency/15.99$</p>
+                    <button>Pay</button>
+                </div>
+
+                <div className ='listOffers'>
+                    <p>Amazing deal</p>
+                    <p>60 currency/35.99$</p>
+                    <button>Pay</button>
+                </div>
+
+                <div className ='listOffers'>
+                    <p>Black sabbath</p>
+                    <p>100 currency/59.99$</p>
+                    <button>Pay</button>
+                </div>
                 
             </div>
         )
