d1d06477bffd1a4cb1b65caa0b077fd773e8889a
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-31 05:10:37 +0100

    created payment component

---
+++ src/components/Payment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Payment.css'
+
+class Payment extends React.Component {
+    render () {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Payment;
