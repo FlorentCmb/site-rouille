dea5ef0868da37c063a7cf31f480b059a43df44c
Elgur <leroy.maxence@yahoo.com>
2019-10-31 10:14:26 +0100

    merge dev to contactBackUp

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Accueil from './components/Accueil'
-import Contact from './components/Contact'
+import Contact from './components/contact/Contact'
 import Summoning from './components/Summoning'
 import About from './components/About'
 import MonsterList from './components/MonsterList';
