aee33d6525ec1fd867be2dcda25554d044ec162c
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-31 10:11:47 +0100

    change a general propertie in accueil

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -4,7 +4,7 @@ import './Accueil.css'
 const Accueil = () => {
     return (
         <div className="Image">
-            <img src="https://i.pinimg.com/originals/fc/d5/eb/fcd5eb28bb45c94dc0b3cfeb9ace217f.jpg" alt=""/>
+            <img className ='accueil-img' src="https://i.pinimg.com/originals/fc/d5/eb/fcd5eb28bb45c94dc0b3cfeb9ace217f.jpg" alt=""/>
             <text className ="text">Site-Rouille</text>
         </div>
     )
