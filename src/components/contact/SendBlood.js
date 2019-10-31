64e80cce0aca62eda230620b86a5fe0b95f914cf
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-31 08:22:25 +0100

    added content to the sendblood

--- src/components/contact/SendBlood.js
+++ src/components/contact/SendBlood.js
@@ -4,8 +4,26 @@ import './SendBlood.css'
 export default class SendBlood extends React.Component {
     render(){
         return(
-            <div>
-                ZZZZZZZZZZZZZZZZZZZZZZZZZZ
+            <div className ='listOffers'>
+                <p>Lack soul shards to summon powerful   monsters ? Try donating your blood !</p>
+
+                <div className='bloodOffers'>
+                    <div className ='bloodText'>
+                        <p>Bloody Mary deal</p>
+                        <p>5 soul shards for 1L of blood !</p>
+                    </div>
+
+                    <div className ='bloodButton'>
+                        <button>Give blood</button>
+                    </div>
+                    
+                    
+                </div>
+
+                
+
+                
+
             </div>
         )
     }
